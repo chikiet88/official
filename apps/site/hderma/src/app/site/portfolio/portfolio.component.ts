3c328e33dce96084e8028355a328ef326724379b
quocbao1983 <quocbao280783@gmail.com>
2023-10-29 15:18:29 +0700

    update

--- apps/site/hderma/src/app/site/portfolio/portfolio.component.ts
+++ apps/site/hderma/src/app/site/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-portfolio',
@@ -7,10 +8,20 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation:ViewEncapsulation.None
 })
 export class PortfolioComponent implements OnInit {
-
+  MyAge:any
+  Exp2:any
+  Exp1:any
   constructor() { }
 
   ngOnInit() {
+    const today = moment();
+    const birthday = moment("1988-05-29");
+    const work1 = moment("2015-04-01");
+    const work2 = moment("2019-03-01");
+    const work3 = moment("2019-03-10");
+    this.MyAge = today.diff(birthday, "years");
+    this.Exp2 = today.diff(work3, "years");
+    this.Exp1 = work2.diff(work1, "years");
   }
 
 }
